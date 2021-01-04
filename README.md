62e36293d7d58eff0e1f05959d1ffc98434cb489
siu-sing <lai.siusing@gmail.com>
2021-01-04 22:55:15 +0800

    refactor code

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Nikko
+
+
+```python
+pipenv install
+pipenv shell
+```
+
+Get `api_id` and `api_hash` from https://my.telegram.org/auth?to=apps
+
+App title and shortname can be random.
+
+To start creating a job, create a new file in the project folder called `config.py`. Enter the job details in the followint format in the file.
+
+```python
+jobs = [
+    {
+        "job_name": "<name of job>",
+        "api_id": "<api_id>",
+        "api_hash": "<api_hash>",
+        "msg_file": "<name of template file>",
+        "recipients": ["list","of","chat_id","like",-345234213,"or","@channel_name"],
+    }
+]
+```
--- nikko.py
+++ nikko.py
@@ -3,18 +3,32 @@ import telethon.sync
 from telethon import functions, types
 from time import sleep
 import random
+import json
+import config as cfg
+from datetime import datetime
 
-# app/account information
-api_id = "2587653"
-api_hash = "0229463569c8e0670949e36807d48798"
-client = TelegramClient("a", api_id, api_hash)
-client.start()
-res = client.get_me().first_name
-print("starting", res)
-rn = random.randint(1,20)
-print("sleep seconds", rn)
-sleep(rn)
-with open("./msg_templates/template_1","r") as msg_template:
-    msg = msg_template.read()
-client.send_message(-438471198, msg)
-client.disconnect()
+def send_message(job_name, api_id, api_hash, msg_file, recipients):
+    
+    # Start TG Client
+    client = TelegramClient(job_name, api_id, api_hash)
+    client.start()
+    username = client.get_me().first_name
+
+    # Pause for random amount of time    
+    sleep(random.randint(1,20))
+    
+    # Read in message template
+    with open(f"./msg_templates/{msg_file}","r") as msg_template:
+        msg = msg_template.read()
+
+    # Broadcast messages to all recipients one by one
+    for r in recipients: 
+        client.send_message(r, msg)
+        print(f"{datetime.now()} : INFO: Message sent from {username} to {r} with {msg_file}")
+
+    client.disconnect()
+    
+for j in cfg.jobs:
+    send_message(j["job_name"], j["api_id"], j["api_hash"],j["msg_file"], j["recipients"])
+    
+
