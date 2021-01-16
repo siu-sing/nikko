7b7bff072656fee03286d55cfb4ab08e607bc98f
siu-sing <lai.siusing@gmail.com>
2021-01-16 22:33:01 +0800

    feat: added error handling for sendmessage

--- nikko.py
+++ nikko.py
@@ -8,33 +8,43 @@ import config as cfg
 from datetime import datetime
 import sys
 
-if (len(sys.argv)<2):
-  print("Usage: python nikko.py <freq>")
-  sys.exit()  
+if len(sys.argv) < 2:
+    print("Usage: python nikko.py <freq>")
+    sys.exit()
+
 
 def send_message(job_name, api_id, api_hash, msg_file, recipients):
-    
+
     # Start TG Client
     client = TelegramClient(job_name, api_id, api_hash)
     client.start()
     username = client.get_me().first_name
 
-    # Pause for random amount of time    
-    sleep(random.randint(1,20))
-    
+    # Pause for random amount of time
+    sleep(random.randint(1, 20))
+
     # Read in message template
-    with open(f"./msg_templates/{msg_file}","r") as msg_template:
+    with open(f"./msg_templates/{msg_file}", "r") as msg_template:
         msg = msg_template.read()
 
     # Broadcast messages to all recipients one by one
-    for r in recipients: 
-        client.send_message(r, msg)
-        print(f"{datetime.now()} : INFO: Message sent from {username} to {r} with {msg_file}")
+    for r in recipients:
+        try:
+            client.send_message(r, msg)
+            print(
+                f"{datetime.now()} : INFO: Message sent from {username} to {r} with {msg_file}"
+            )
+        except Exception as e:
+            print(
+                f"{datetime.now()} : FAILED: Unable to send from {username} to {r} with {msg_file}"
+            )
+            print(f"ERROR: {e}")
 
     client.disconnect()
 
+
 freq = sys.argv[1]
 for j in cfg.jobs[freq]:
-    send_message(j["job_name"], j["api_id"], j["api_hash"], j["msg_file"], j["recipients"])
-    
-
+    send_message(
+        j["job_name"], j["api_id"], j["api_hash"], j["msg_file"], j["recipients"]
+    )
